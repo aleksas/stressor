28335bece4f330507ae4e56f022611928e10096c
Aleksas Pielikis <ant.kampo@gmail.com>
2018-10-03 16:53:13 +0300

    regex fix

--- Build.ps1
+++ Build.ps1
@@ -1,5 +1,37 @@
 $ErrorActionPreference = 'Stop';
 Write-Host Starting build
 
+if ($isWindows) {
+
 msbuild .\native\source\.VS2017\PhonologyEngine.sln /property:Platform=x86 /p:Configuration=Release  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 msbuild .\native\source\.VS2017\PhonologyEngine.sln /property:Platform=x64 /p:Configuration=Release  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+
+} else {
+
+	cd native/source
+
+	mkdir -p ../../phonology_engine/Linux_x86
+
+	g++ -fPIC -m32 -g \
+	  -I ../include/ \
+	  Transkr.cpp transcrLUSS.cpp Skiemen.cpp Kircdb.cpp fv2id.cpp \
+	  ArKirciuoti.cpp strtokf.cpp stringWithLetterPosition.cpp TextNormalization.cpp \
+	  -shared -o ../../phonology_engine/Linux_x86/libPhonologyEngine.so -Wno-write-strings
+	  
+	appveyor PushArtifact ../../phonology_engine/Linux_x86/libPhonologyEngine.so
+
+	mkdir -p ../../phonology_engine/Linux_x86_64
+
+	g++ -fPIC -g \
+	  -I ../include/ \
+	  Transkr.cpp transcrLUSS.cpp Skiemen.cpp Kircdb.cpp fv2id.cpp \
+	  ArKirciuoti.cpp strtokf.cpp stringWithLetterPosition.cpp TextNormalization.cpp \
+	  -shared -o ../../phonology_engine/Linux_x86_64/libPhonologyEngine.so -Wno-write-strings
+
+	appveyor PushArtifact ../../phonology_engine/Linux_x86_64/libPhonologyEngine.so
+
+	foreach ($artifactName in $artifacts.keys) {
+	  echo $artifacts[$artifactName]
+	}
+
+}
