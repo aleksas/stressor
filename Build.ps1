5225fc8ef3ae8c5b722984ac2eda7d9aa9386a02
Aleksas Pielikis <ant.kampo@gmail.com>
2018-10-03 17:43:03 +0300

    regex fix

--- Build.ps1
+++ Build.ps1
@@ -3,11 +3,19 @@ Write-Host Starting build
 
 if ($isWindows) {
 
-msbuild .\native\source\.VS2017\PhonologyEngine.sln /property:Platform=x86 /p:Configuration=Release  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-Push-AppveyorArtifact ./phonology_engine/Win32_x86/PhonologyEngine.dll
+	msbuild .\native\source\.VS2017\PhonologyEngine.sln /property:Platform=x86 /p:Configuration=Release  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	Push-AppveyorArtifact ./phonology_engine/Win32_x86/PhonologyEngine.dll -FileName Win32_x86/PhonologyEngine.dll
 
-msbuild .\native\source\.VS2017\PhonologyEngine.sln /property:Platform=x64 /p:Configuration=Release  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-Push-AppveyorArtifact ./phonology_engine/Win64_x64/PhonologyEngine.dll
+	foreach ($artifactName in $artifacts.keys) {
+	  echo $artifacts[$artifactName]
+	}
+	
+	msbuild .\native\source\.VS2017\PhonologyEngine.sln /property:Platform=x64 /p:Configuration=Release  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	Push-AppveyorArtifact ./phonology_engine/Win64_x64/PhonologyEngine.dll -FileName Win64_x64/PhonologyEngine.dll
+	
+	foreach ($artifactName in $artifacts.keys) {
+	  echo $artifacts[$artifactName]
+	}
 
 } else {
 
@@ -23,8 +31,12 @@ Push-AppveyorArtifact ./phonology_engine/Win64_x64/PhonologyEngine.dll
 	  ArKirciuoti.cpp strtokf.cpp stringWithLetterPosition.cpp TextNormalization.cpp \
 	  -shared -o ../../phonology_engine/Linux_x86/libPhonologyEngine.so -Wno-write-strings
 	  
-	Push-AppveyorArtifact ../../phonology_engine/Linux_x86/libPhonologyEngine.so
+	Push-AppveyorArtifact ../../phonology_engine/Linux_x86/libPhonologyEngine.so -FileName Linux_x86/libPhonologyEngine.so
 
+	foreach ($artifactName in $artifacts.keys) {
+	  echo $artifacts[$artifactName]
+	}
+	
 	mkdir -p ../../phonology_engine/Linux_x86_64
 
 	g++ -fPIC -g \
@@ -33,7 +45,7 @@ Push-AppveyorArtifact ./phonology_engine/Win64_x64/PhonologyEngine.dll
 	  ArKirciuoti.cpp strtokf.cpp stringWithLetterPosition.cpp TextNormalization.cpp \
 	  -shared -o ../../phonology_engine/Linux_x86_64/libPhonologyEngine.so -Wno-write-strings
 
-	Push-AppveyorArtifact ../../phonology_engine/Linux_x86_64/libPhonologyEngine.so
+	Push-AppveyorArtifact ../../phonology_engine/Linux_x86_64/libPhonologyEngine.so -FileName Linux_x86_64/libPhonologyEngine.so
 
 	foreach ($artifactName in $artifacts.keys) {
 	  echo $artifacts[$artifactName]
