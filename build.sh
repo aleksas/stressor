89452ebc799be3400e8842fea1978988b38f554f
Aleksas Pielikis <ant.kampo@gmail.com>
2018-10-03 15:34:44 +0300

    Linux build fix.

--- build.sh
+++ build.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-g++ args -fPIC -m32 -g \
+sudo apt-get install -y libc6-dev-i386
+
+cd native/source
+
+mkdir -p ../../phonology_engine/Linux_x86
+
+g++ -fPIC -m32 -g \
+  -I ../include/ \
   Transkr.cpp transcrLUSS.cpp Skiemen.cpp Kircdb.cpp fv2id.cpp \
   ArKirciuoti.cpp strtokf.cpp stringWithLetterPosition.cpp TextNormalization.cpp \
   -shared -o ../../phonology_engine/Linux_x86/libPhonologyEngine.so -Wno-write-strings
 
-g++ args -fPIC -g \
+mkdir -p ../../phonology_engine/Linux_x86_64
+
+g++ -fPIC -g \
+  -I ../include/ \
   Transkr.cpp transcrLUSS.cpp Skiemen.cpp Kircdb.cpp fv2id.cpp \
   ArKirciuoti.cpp strtokf.cpp stringWithLetterPosition.cpp TextNormalization.cpp \
   -shared -o ../../phonology_engine/Linux_x86_64/libPhonologyEngine.so -Wno-write-strings
