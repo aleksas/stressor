a4eedb63a58eb13ca9a42d66cc6aa17adadedd9d
Aleksas Pielikis <ant.kampo@gmail.com>
2018-10-03 20:55:21 +0300

    Fix

--- Build.ps1
+++ Build.ps1
@@ -55,7 +55,7 @@ if ($isWindows) {
 	
 	########## Build WHEEL ##########
 	
-	sudo apt install python3-pip
+	sudo apt install python3-pip -y
 	sudo python3 -m pip install --user --upgrade setuptools wheel
 	
 	python3 setup.py sdist bdist_wheel
