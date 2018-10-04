44c6e50c3a5e7642a0279fb84ef7c5f97392879b
Aleksas Pielikis <ant.kampo@gmail.com>
2018-10-04 19:22:58 +0300

    test

--- phonology_engine/tests/tests.py
+++ phonology_engine/tests/tests.py
@@ -5,9 +5,4 @@ from pprint import pprint
 
 def test_laba_diena():
     pe = PhonologyEngine()
-    res = pe.process('Laba diena')
-    print('asd')
-
-
-    
-    # TODO: make more tests
+    pe.process('Laba diena')
