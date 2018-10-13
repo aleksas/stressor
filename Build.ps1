6d385898154176f2ac90a2e057862ddcd171a176
Aleksas Pielikis <ant.kampo@gmail.com>
2018-10-13 12:12:07 +0300

    Trying to figure out build scripts

--- Build.ps1
+++ Build.ps1
@@ -14,10 +14,10 @@ if ($init) {
 	Write-Host Initializing
 
 	if ($isWindows) {
-		python -m pip install -y --upgrade pip setuptools wheel nose six
+		python -m pip install --upgrade pip setuptools wheel nose six
 	} else {
 		sudo apt install python-pip -y	
-		python -m pip install -y --user --upgrade pip setuptools wheel nose six twine
+		yes | python -m pip install --user --upgrade pip setuptools wheel nose six twine
 	}
 }
 
